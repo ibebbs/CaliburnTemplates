236ff94ab2be61309307886de5544e4a1ad43f94
Ian Bebbington <github@bebbs.co.uk>
2015-09-30 12:40:11 +0100

    Initial commit

---
+++ src/Caliburn.Micro.Universal.Windows/App.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Caliburn.Micro.Universal.Windows
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : CaliburnApplication
+    {
+        private WinRTContainer _container;
+
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            Microsoft.ApplicationInsights.WindowsAppInitializer.InitializeAsync(
+                Microsoft.ApplicationInsights.WindowsCollectors.Metadata |
+                Microsoft.ApplicationInsights.WindowsCollectors.Session);
+
+            InitializeComponent();
+            Suspending += OnSuspending;
+        }
+
+        protected override void Configure()
+        {
+            _container = new WinRTContainer();
+
+            _container.RegisterWinRTServices();
+
+            // Make sure to register your containers here
+            _container.PerRequest<ViewModels.IShellViewModel, ViewModels.ShellViewModel>();
+        }
+        
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+#if DEBUG
+            if (Debugger.IsAttached)
+            {
+                DebugSettings.EnableFrameRateCounter = true;
+            }
+#endif
+            // I am launching my main view here
+            DisplayRootViewFor<ViewModels.IShellViewModel>();
+        }
+ 
+        protected override void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+
+        protected override void PrepareViewFirst(Frame rootFrame)
+        {
+            _container.RegisterNavigationService(rootFrame);
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+    }
+}
---
+++ src/Caliburn.Micro.Universal.Windows/ViewModels/ShellViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caliburn.Micro.Universal.Windows.ViewModels
+{
+    public interface IShellViewModel : IScreen
+    {
+        string Text { get; }
+    }
+
+    internal class ShellViewModel : Screen, IShellViewModel
+    {
+        public ShellViewModel()
+        {
+            Text = typeof(App).Namespace;
+        }
+
+        public string Text { get; private set; }
+    }
+}
